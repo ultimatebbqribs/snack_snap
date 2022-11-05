0cd432ea94a9c159bb14b7522d2995215786d192
alexbarker <alexandersbarker89@gmail.com>
2022-11-05 18:05:09 +1100

    signin hash checking function created

--- models/db.py
+++ models/db.py
@@ -5,7 +5,6 @@ def sql_select(db_query, params=[]):
     cur = conn.cursor()
     cur.execute(db_query,params)
     results = cur.fetchall()
-    
     cur.close()
     conn.close()
     return results
--- server.py
+++ server.py
@@ -32,11 +32,29 @@ def sign_up_action():
     username = request.form.get('username')
     email = request.form.get('email')
     password = request.form.get('password')
-    pw_hash = bcrypt.generate_password_hash(password)
+    pw_hash = bcrypt.generate_password_hash(password).decode('utf8')
     sql_write('INSERT INTO users(username,email,pw_hash) VALUES(%s,%s,%s)', [username,email,pw_hash])
-    
     return redirect('/')
 
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html')
+
+@app.route('/sign_in_action',methods=['POST', 'GET'])
+def sign_in_action():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    results = sql_select('SELECT email, pw_hash FROM users WHERE email = %s', [str(email)])
+    for row in results:
+        db_email, pw_hash = row
+    print(db_email, pw_hash)
+    if bcrypt.check_password_hash(pw_hash, password):
+        print(f'successful login')
+    else:
+        (f'username or password incorrect')
+
+
+    return redirect('/')
 
 @app.route('/add_post', methods=['POST', 'GET'])
 def add_post():
@@ -59,4 +77,4 @@ def img_upload():
 
 if __name__ == '__main__':
     from dotenv import load_dotenv
-    app.run(port=5013, debug=True)
+    app.run(port=5014, debug=True)
