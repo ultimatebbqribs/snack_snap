cde92cb0599207f9496cfdf4af400885ab4bce3f
alexbarker <alexandersbarker89@gmail.com>
2022-11-05 15:36:03 +1100

    basic styling on signup form

--- schema.sql
+++ schema.sql
@@ -4,20 +4,25 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    PasswordHash BINARY(64),
+    PasswordHash VARCHAR(255)
     );
 
 CREATE TABLE posts(
-    username VARCHAR(50) FOREIGN KEY REFERENCES users(username),
+    user_id INTEGER,
     img_url VARCHAR(255),
     img_title VARCHAR(255),
+    comment_date 
     up_votes INTEGER,
     down_votes INTEGER, 
     post_id SERIAL PRIMARY KEY,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE post_comments(
-    post_id FOREIGN KEY REFERENCES posts(post_id),
+    post_id SERIAL PRIMARY KEY,
     comment VARCHAR(255),
     username VARCHAR(255),
+    FOREIGN KEY (post_id) REFERENCES posts(post_id)
+    
 );
+
