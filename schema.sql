0bd88c69f54a2048dfc238d52488a63843f68f15
alexbarker <alexandersbarker89@gmail.com>
2022-11-06 10:39:12 +1100

    form style

--- schema.sql
+++ schema.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS users, posts, post_comments;
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    username VARCHAR(50) UNIQUE,
+    email VARCHAR(100) UNIQUE,
     pw_hash TEXT
     );
 
