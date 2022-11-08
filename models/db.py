16cffd443b1963e40215c13c117d8379475bb22f
alexbarker <alexandersbarker89@gmail.com>
2022-11-08 20:42:44 +1100

    heroku db

--- models/db.py
+++ models/db.py
@@ -3,7 +3,7 @@ import os
 DB_URL = os.environ.get('DATABASE_URL', 'dbname=snack_snap')
 
 def sql_select(db_query, params=[]):
-    conn = psycopg2.connect('dbname=DB_URL')
+    conn = psycopg2.connect(DB_URL)
     cur = conn.cursor()
     cur.execute(db_query,params)
     results = cur.fetchall()
@@ -12,7 +12,7 @@ def sql_select(db_query, params=[]):
     return results
 
 def sql_write(db_query, params=[]):
-    conn = psycopg2.connect('dbname=DB-URL')
+    conn = psycopg2.connect(DB_URL)
     cur = conn.cursor()
     cur.execute(db_query,params)
     conn.commit()
